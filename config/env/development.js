636af3263aff95c7b363ee31fdc0f4c6aa53eb5f
pratik60 <pratik60@gmail.com>
2015-01-07 20:45:12 +0530

    Basic initialisation of the permission package

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/ecommercetoolbox-dev',
   debug: true,
-//  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
+  //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
   aggregate: false,
   mongoose: {
     debug: false
--- packages/system/server/controllers/index.js
+++ packages/system/server/controllers/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var mean = require('meanio');
+var mean = require('meanio'),
+  mongoose = require('mongoose'),
+  Variable = mongoose.model('Variable');
 
 exports.render = function(req, res) {
 
@@ -14,20 +16,36 @@ exports.render = function(req, res) {
     });
   }
 
+  function listPermissions(allPermission) { // checking for user role and permission
+    var permission = [];
+    allPermission[0].data.forEach(function(value) {
+      value.roles.forEach(function(role) {
+        if (req.user.roles.indexOf(role) > -1)
+          permission.push(value.permission);
+      });
+    });
+    return permission;
+  }
+
   function isAdmin() {
     return req.user && req.user.roles.indexOf('admin') !== -1;
   }
 
-  // Send some basic starting info to the view
-  res.render('index', {
-    user: req.user ? {
-      name: req.user.name,
-      _id: req.user._id,
-      username: req.user.username,
-      roles: req.user.roles
-    } : {},
-    modules: modules,
-    isAdmin: isAdmin,
-    adminEnabled: isAdmin() && mean.moduleEnabled('mean-admin')
+  var query = Variable.find({
+    name: 'permission'
+  });
+  query.exec(function(err, allPermission) {
+    res.render('index', {
+      user: req.user ? {
+        name: req.user.name,
+        _id: req.user._id,
+        username: req.user.username,
+        roles: req.user.roles,
+        permission: listPermissions(allPermission) // new permission variable setup
+      } : {},
+      modules: modules,
+      isAdmin: isAdmin,
+      adminEnabled: isAdmin() && mean.moduleEnabled('mean-admin')
+    });
   });
 };
