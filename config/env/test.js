5e26986e529a1748d272fd3d6ab008ae407ed437
pratik60 <pratik60@gmail.com>
2015-01-09 12:18:26 +0530

    Mean ng

--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/ecommercetoolbox-test',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/meanng-test',
   http: {
     port: 3001
   },
